703c1ac943f6d90813ead06f79379d2e13abcb34
testrae <mgokberkoguz2@gmail.com>
2021-06-24 16:48:52 +0300

    model tests

--- rest_api/tests.py
+++ rest_api/tests.py
@@ -1,3 +1,13 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import ToDoItem
 from django.test import TestCase
 
-# Create your tests here.
+class ModelTests(TestCase):
+
+    def test_create_model_ToDo(self):
+        data = {'text': 'Ev işleri yapılacak','done': False }
+        item = ToDoItem(**data).save()
+        item = ToDoItem.objects.get(text="Ev işleri yapılacak")
+        self.assertIsInstance(item, ToDoItem)
